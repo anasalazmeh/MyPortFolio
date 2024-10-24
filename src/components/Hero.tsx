71d89fd3d60c8d8a3fca2763589a0f4afd3a8fe5
anasalazmeh <anoosalazmeh@gmail.com>
2024-10-24 21:10:44 +0300

    fix the Errors 5

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 // import { ComputersCanvas } from "./canvas";
@@ -21,7 +20,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-        <ComputersCanvas  />
+      <ComputersCanvas />
       {/* <ComputersCanvas /> */}
       {/* <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
