8c88529927d695c334fa931770d81ec21306847e
anasalazmeh <anoosalazmeh@gmail.com>
2024-11-02 18:34:47 +0300

    fix the number ball

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
 import { technologies } from "../constants";
 import Container from "../hoc/Container";
 import { BallCanvas } from "./canvas";
 const Tech = () => {
+  const [mobile, setMonile] = useState(false);
+  useEffect(() => {
+    if (window.screenX < 767) {
+      setMonile(true);
+    }
+  }, []);
   return (
     <Container>
       <div className="flex flex-row flex-wrap justify-center gap-10">
-        {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name} >
-            <BallCanvas icon={technology.icon} />
-          </div>
-        ))}
+        {technologies.map((technology, index) => {
+          if (mobile) {
+            if (index < 8) {
+              return (
+                <div className="w-28 h-28" key={technology.name}>
+                  <BallCanvas icon={technology.icon} />
+                </div>
+              );
+            }
+          } else {
+            return (
+              <div className="w-28 h-28" key={technology.name}>
+                <BallCanvas icon={technology.icon} />
+              </div>
+            );
+          }
+        })}
       </div>
     </Container>
   );
